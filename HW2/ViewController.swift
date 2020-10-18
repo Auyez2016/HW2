47bf429fdb63290bd54c48b23d53e46d9c44a43d
Auyez <auyez2016@yandex.ru>
2020-10-18 20:39:25 +0600

    Final

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewColors: UIView!
+    
+    @IBOutlet var redNumbers: UILabel!
+    @IBOutlet var greenNumbers: UILabel!
+    @IBOutlet var blueNumbers: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    private var redColor: CGFloat = 0
+    private var greenColor: CGFloat = 0
+    private var blueColor: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func redSliderAction() {
+        redColor = CGFloat(redSlider.value)
+        redNumbers.text = String(format: "%.2f", redSlider.value)
+        coloring()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenColor = CGFloat(greenSlider.value)
+        greenNumbers.text = String(format: "%.2f", greenSlider.value)
+        coloring()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueColor = CGFloat(blueSlider.value)
+        blueNumbers.text = String(format: "%.2f", blueSlider.value)
+        coloring()
+    }
+    
+    private func coloring() {
+        viewColors.backgroundColor = UIColor(displayP3Red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    
 }
+
 
